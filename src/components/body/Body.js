46c1d6be15ca1d6969d3181c7b07e871f3ef0f3a
Rajput-Suraj <surajarajput@gmail.com>
2020-11-24 14:19:11 +0530

    Completed working on header banner with user's data fetched from spotify.

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./Body.css";
 import Header from "../header/Header";
+import { useDataLayervalue } from "../../DataLayer";
 
 const Body = ({ spotify }) => {
+    const [{ discover_weekly }, dispatch] = useDataLayervalue();
     return (
         <div className="body">
             <Header spotify={spotify} />
+
+            <div className="body__info">
+                <img src={discover_weekly?.images[0].url} alt="" />
+                <div className="body__infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>Discover Weekly</h2>
+                    <p>{discover_weekly?.description}</p>
+                </div>
+            </div>
         </div>
     );
 };
