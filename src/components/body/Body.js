5f390bb675a6cc6f7735e3f822f33f9675b48e4b
Rajput-Suraj <surajarajput@gmail.com>
2020-11-24 14:31:58 +0530

    Worked on playlist options with styling.

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./Body.css";
 import Header from "../header/Header";
 import { useDataLayervalue } from "../../DataLayer";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 
 const Body = ({ spotify }) => {
     const [{ discover_weekly }, dispatch] = useDataLayervalue();
@@ -15,6 +18,14 @@ const Body = ({ spotify }) => {
                     <strong>PLAYLIST</strong>
                     <h2>Discover Weekly</h2>
                     <p>{discover_weekly?.description}</p>
+                </div>
+            </div>
+
+            <div className="body__songs">
+                <div className="body__icons">
+                    <PlayCircleFilledIcon className="body__shuffle" />
+                    <FavoriteIcon fontSize="large" />
+                    <MoreHorizIcon />
                 </div>
             </div>
         </div>
